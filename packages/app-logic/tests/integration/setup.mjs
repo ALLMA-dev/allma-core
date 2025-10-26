c784cb1ce9713c0e62f3f9bbdbdd28af719ecb29
Nick Mellon <nick@kavunenko.com>
2025-10-27 00:56:38 +0100

    chore: debug npm run test setup in CI 2

--- packages/app-logic/tests/integration/setup.mjs
+++ packages/app-logic/tests/integration/setup.mjs
@@ -10,14 +10,12 @@ export default async () => {
   process.env[ENV_VAR_NAMES.ALLMA_EXECUTION_TRACES_BUCKET_NAME] = defaultConfig.allmaExecutionTracesBucketName;
   process.env[ENV_VAR_NAMES.AI_API_KEY_SECRET_ARN] = defaultConfig.aiApiKeySecretArn;
 
-  console.log('\nRegion is set to:', process.env.AWS_REGION_CI || 'not set');
+  console.log('\nRegion is set to:', process.env.AWS_REGION || 'not set');
   
   // Add any other required environment variables here
-  if (process.env.AWS_ACCESS_KEY_ID_CI && process.env.AWS_SECRET_ACCESS_KEY_CI && process.env.AWS_REGION_CI) {
-    console.log('CI environment detected, setting AWS credentials from environment variables.');
-    process.env.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID_CI;
-    process.env.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY_CI;
-    process.env.AWS_REGION = process.env.AWS_REGION_CI;
+  
+  if (process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY && process.env.AWS_REGION) {
+    console.log('CI environment detected. Using provided AWS credentials.');
   }
   
   console.log(`Using DynamoDB Config Table: ${process.env[ENV_VAR_NAMES.ALLMA_CONFIG_TABLE_NAME]}`);
